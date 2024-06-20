a4cac04ab4ec4fddfbc12bb863a91bd84128d20c
AmritRoy07 <140098423+AmritRoy07@users.noreply.github.com>
2024-06-20 14:13:25 +0530

    js

--- script.js
+++ script.js
@@ -28,37 +28,13 @@ const subjectsContainer = document.querySelector('.subjects-container');
 const arrowLeft = document.querySelector('.arrow.left');
 const arrowRight = document.querySelector('.arrow.right');
 
-const pixelStep = 1; // Adjust this value as needed
-
 arrowRight.addEventListener('click', () => {
-    subjectsContainer.scrollBy({ left: pixelStep, behavior: 'smooth' });
+    subjectsContainer.scrollBy({ left: 3000, behavior: 'smooth' });
 });
 
 arrowLeft.addEventListener('click', () => {
-    subjectsContainer.scrollBy({ left: -pixelStep, behavior: 'smooth' });
+    subjectsContainer.scrollBy({ left: -3000, behavior: 'smooth' });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const subjectsContainer = document.querySelector('.subjects-container');
-    const scrollStep = subjectsContainer.offsetWidth; // Width of one image element
 
-    const leftArrow = document.querySelector('.arrow.left');
-    const rightArrow = document.querySelector('.arrow.right');
-    let currentIndex = 0; // Current index of the displayed image
-
-    leftArrow.addEventListener('click', function() {
-        if (currentIndex > 0) {
-            currentIndex--;
-            subjectsContainer.scrollTo({ left: currentIndex * scrollStep, behavior: 'smooth' });
-        }
-    });
-
-    rightArrow.addEventListener('click', function() {
-        const maxIndex = subjectsContainer.children.length - 1;
-        if (currentIndex < maxIndex) {
-            currentIndex++;
-            subjectsContainer.scrollTo({ left: currentIndex * scrollStep, behavior: 'smooth' });
-        }
-    });
-});
 
